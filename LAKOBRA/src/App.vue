30384aa3c7c2439a340d95458e8f3d4d0c810eea
Fran3457 <francisco.ruizro@elorrieta-errekamari.com>
2026-03-23 12:31:59 +0100

    fusion

--- LAKOBRA/src/App.vue
+++ LAKOBRA/src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-import Header from './components/Header.vue';
+import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <div>
-        <Header />
-        <router-view />
-    </div>
   <div class="app-container">
+    <div>
+      <Header />
+      <router-view />
+    </div>
     <main class="content">
       <h1>Bienvenido a Lakobra</h1>
-      <p>Esto es contenido de prueba para ver cómo el footer se queda abajo.</p>
+      <p>iNFO LAKOBRA.</p>
     </main>
 
     <Footer />
@@ -22,12 +22,12 @@ import Footer from './components/Footer.vue'
 .app-container {
   display: flex;
   flex-direction: column;
-  min-height: 100vh; /* Ocupa el 100% de la altura de la ventana */
-  width: 100%;       /* Ocupa todo el ancho */
+  min-height: 100vh; /* Ocupa el 100% de la altura de la ventana /
+  width: 100%; / Ocupa todo el ancho /
 }
 
 .content {
-  flex: 1;           /* Esto hace que el main crezca y empuje al footer al final */
+  flex: 1; / Esto hace que el main crezca y empuje al footer al final */
   padding: 40px 20px;
 }
 </style>
